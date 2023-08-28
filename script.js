4886276dadce751dc9735601e34b3f5f9ca22e9d
Swetha Udayagiri <106729744+SwethaUdayagiri@users.noreply.github.com>
2023-08-28 21:16:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+	var s="";
+	while(num>1){
+		s=(num%2)+s;
+		num=num/2;
+	}
+	return s;
 }
 
 window.decimalToBinary = decimalToBinary;
